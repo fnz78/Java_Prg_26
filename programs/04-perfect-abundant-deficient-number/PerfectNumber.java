4ed5968251c49da7c8ddd1718225b3ab2197833b
Nazim_fnz78 <farhannazimfnz@gmail.com>
2026-04-27 20:52:30 +0530

    Create PerfectNumber.java

---
+++ programs/04-perfect-abundant-deficient-number/PerfectNumber.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+
+        if (sum == n) {
+            System.out.println(n + " is a Perfect Number");
+        } else if (sum > n) {
+            System.out.println(n + " is an Abundant Number");
+        } else {
+            System.out.println(n + " is a Deficient Number");
+        }
+
+        sc.close();
+    }
+}
